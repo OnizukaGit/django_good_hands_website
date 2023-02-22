3eff432af2d7d63e795914e072453274458ba829
OnizukaGit <kamil.kirko.wro@gmail.com>
2023-02-22 23:32:50 +0100

    counting_bags_and_organizations

--- website/views.py
+++ website/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from website.models import Donation
+from django.db.models import Sum
 
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, "website/index.html")
+        institution = Donation.objects.values_list('quantity', flat=True)
+        total_quantity = Donation.objects.aggregate(total=Sum('quantity'))['total']
+        return render(request, "website/index.html", context={"institution": institution,
+                                                              "total_quantity": total_quantity})
 
 
 class AddDonation(View):
