8d6c4657926c95005561edadfdfb7d8e8e639725
OnizukaGit <kamil.kirko.wro@gmail.com>
2023-02-24 23:57:40 +0100

    add_details_fundacion_in_html

--- website/views.py
+++ website/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.views import View
-from website.models import Donation
+from website.models import Donation, Institution, Category
 from django.db.models import Sum
 
 
 class LandingPage(View):
     def get(self, request):
-        institution = Donation.objects.values_list('quantity', flat=True)
+        institution = Institution.objects.all()
+        categories = Category.objects.all()
+        institution_quantity = Donation.objects.values_list('quantity', flat=True)
         total_quantity = Donation.objects.aggregate(total=Sum('quantity'))['total']
-        return render(request, "website/index.html", context={"institution": institution,
-                                                              "total_quantity": total_quantity})
-
+        return render(request, "website/index.html", context={
+            "institution": institution,
+            "categories": categories,
+            "institution_quantity": institution_quantity,
+            "total_quantity": total_quantity,
+        })
 
 class AddDonation(View):
     def get(self, request):
