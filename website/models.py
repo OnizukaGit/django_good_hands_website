5646e04ae1e49731d6bf61bd30ae08c550284006
OnizukaGit <kamil.kirko.wro@gmail.com>
2023-03-04 02:16:18 +0100

    improved_pagination_delete_css_test

--- website/models.py
+++ website/models.py
@@ -50,6 +50,3 @@ class Donation(models.Model):
     pick_up_time = models.TimeField()
     pick_up_comment = models.CharField(max_length=128)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
-
-
