a569fcd755586cd0ccb18bca4d904c20269f7fca
OnizukaGit <kamil.kirko.wro@gmail.com>
2023-03-06 00:26:53 +0100

    improvement_register

--- website/forms.py
+++ website/forms.py
@@ -1,17 +1,20 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
 
 User = get_user_model()
 
 
-class RegisterForms(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, label="Hasło")
+class RegisterForms(UserCreationForm):
+    email = forms.EmailField(required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Hasło")
     password2 = forms.CharField(widget=forms.PasswordInput, label="Powtórz hasło")
 
     class Meta:
         model = User
         fields = [
-            "username", "first_name", "last_name", "email"
+            "email", "first_name", "last_name"
         ]
 
     def clean_email(self):
@@ -19,14 +22,5 @@ class RegisterForms(forms.ModelForm):
         if User.objects.filter(username=email).exists():
             raise forms.ValidationError("Ten email jest już zarejestrowany.")
         return email
-
-    def clean_password2(self):
-        password = self.cleaned_data.get('password')
-        password2 = self.cleaned_data.get('password2')
-        if password and password2 and password != password2:
-            raise forms.ValidationError("Hasła nie są identyczne.")
-        return password2
-
-
 
 
